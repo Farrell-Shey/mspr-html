8c214154aa7a202363531e5c4e7c476f1de5a9fb
Victor Grollier <victor.grollier@gmai.com>
2020-02-04 09:53:29 +0100

    update: contact form

--- contact.php
+++ contact.php
@@ -1,5 +1,42 @@
 <?php require_once 'includes/header.php'; ?>
 
+<form>
 
+    <div class="row">
+
+        <div class="col">
+
+
+            <label for="first-name">Prénom</label>
+            <input id="first-name" type="text" class="form-control" placeholder="Prénom">
+
+        </div>
+
+        <div class="col">
+
+            <label for="last-name">Nom</label>
+            <input id="last-name" type="text" class="form-control" placeholder="Nom">
+
+        </div>
+
+    </div>
+
+    <div class="form-group">
+
+        <label for="email">Email</label>
+        <input type="email" class="form-control" id="email" placeholder="Email@example.com">
+
+    </div>
+
+    <div class="form-group">
+
+        <label for="phone">Numéros de Téléphone</label>
+        <input type="number" class="form-control" id="phone" placeholder="06 XX XX XX XX">
+
+    </div>
+
+    <button type="submit" class="btn btn-primary">ENVOYER</button>
+
+</form>
 
 <?php require_once 'includes/footer.php'; ?>
--- public/js/app.js
+++ public/js/app.js
@@ -29,8 +29,8 @@ __webpack_require__(/*! bootstrap */ "./node_modules/bootstrap/dist/js/bootstrap
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\wamp64\www\mspr-html\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\wamp64\www\mspr-html\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\wamp64\www\mspr-html\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\wamp64\www\mspr-html\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
