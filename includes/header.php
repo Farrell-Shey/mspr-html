0b28ece7fa38bc7f7fcbb0654df53f39d02e4c3c
Elliot <elliot.decle@gmail.com>
2020-02-04 14:22:45 +0100

    update : produits

--- includes/header.php
+++ includes/header.php
@@ -11,7 +11,9 @@
 <body>
 <header>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="#">
+            <img src="public/image/logo.png" alt="logo de caviar royal gift">
+        </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
