fa134dbe423a00333f6560aab8e573866ae91d14
Victor Grollier <victor.grollier@gmai.com>
2020-02-04 11:14:19 +0100

    update: contact form

--- contact.php
+++ contact.php
@@ -1,41 +1,41 @@
 <?php require_once 'includes/header.php'; ?>
 
 <form>
+    <div class="container" >
 
-    <div class="row">
+        <div class="form-group">
 
-        <div class="col">
+            <div class="row">
 
+                <div class="col">
 
-            <label for="first-name">Prénom</label>
-            <input id="first-name" type="text" class="form-control" placeholder="Prénom">
+                    <label for="first-name">Prénom</label>
+                    <input id="first-name" type="text" class="form-control" placeholder="Prénom">
 
+                </div>
+
+                <div class="col">
+
+                    <label for="last-name">Nom</label>
+                    <input id="last-name" type="text" class="form-control" placeholder="Nom">
+
+                </div>
+            </div>
         </div>
 
-        <div class="col">
-
-            <label for="last-name">Nom</label>
-            <input id="last-name" type="text" class="form-control" placeholder="Nom">
-
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" class="form-control" id="email" placeholder="Email@example.com">
         </div>
 
-    </div>
+        <div class="form-group">
+            <label for="phone">Numéros de Téléphone</label>
+            <input type="number" class="form-control" id="phone" placeholder="06 XX XX XX XX">
+        </div>
 
-    <div class="form-group">
-
-        <label for="email">Email</label>
-        <input type="email" class="form-control" id="email" placeholder="Email@example.com">
-
-    </div>
-
-    <div class="form-group">
-
-        <label for="phone">Numéros de Téléphone</label>
-        <input type="number" class="form-control" id="phone" placeholder="06 XX XX XX XX">
+        <button type="submit" class="btn btn-outline-light">ENVOYER</button>
 
     </div>
-
-    <button type="submit" class="btn btn-primary">ENVOYER</button>
 
 </form>
 
