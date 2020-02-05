90e1c633bafe80e13ea3bf54ed03bb8f32e184cb
Elliot <elliot.decle@gmail.com>
2020-02-05 11:46:34 +0100

    update : animation stylé

--- produit.php
+++ produit.php
@@ -7,7 +7,7 @@
                 <img src="public/image/tourbillon-carbon-2.png">
             </div>
         </div>
-        <div class="col">
+        <div class="col-3">
             <div class="card-sm">
                 <img src="public/image/tourbillon-zoom-1.png">
             </div>
@@ -15,9 +15,12 @@
                 <img src="public/image/tourbillon-zoom-2.png">
             </div>
         </div>
-        <div class="col">
-            <h1>le nom du téléphone</h1>
-            <p>lorem ipsum ...</p>
+        <div class="col mr-1">
+            <div class="typewriter">
+            <h1>GRANDE COMPLICATION BLACK SKELETON</h1>
+            </div>
+            <p class="text-white">Iphone forgé dans un cimetière de héros, inconnu de tous crée à l'aide d'or jaune et noir. Les héros de l'ancien temps vous porteront au sein de vos journées quand vous aurez cette oeuvre dans vos mains.
+                iphone mars : Cet iphone forgé par le dieu de la guerre au cœur de sa propre planète, il vous aura laissé un présent visible sur cet iphone pour vous souvenir de la divinité que vous aurez entre vos mains.</p>
         </div>
     </div>
 </section>
