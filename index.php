896bbcfe839bc43f11a9ddc7325802a075fe983b
Elliot <elliot.decle@gmail.com>
2020-02-06 16:56:23 +0100

    update: index

--- index.php
+++ index.php
@@ -6,13 +6,22 @@
         <img src="public/image/tourbillon-carbon-2.png" alt="grand complication tourbillon carbon" class="img">
     </div>
 </section>
-<section class="bg-fix" style="background-image: url(#)">
+<section class="bg-fix"  style="background-image: url(public/image/bg-gold.jpg)">
     <div class="container-fluid">
-        <h2>Une finitions Inégalée</h2>
+        <h2 class="h2">Une finition Inégalée</h2>
     </div>
     <div class="bg-dark">
         <div class="container-fluid">
-            <blockquote class="blockquote">j'adore cet iphone</blockquote>
+            <blockquote class="blockquote">
+                <div class="row">
+                    <div class="col">
+                        <img class="tweet" src="public/image/tweet.png" alt="avis de bernard">
+                    </div>
+                    <div class="col">
+                        <img class="tweet" src="public/image/tweet2.png" alt="avis de bernard">
+                    </div>
+                </div>
+            </blockquote>
         </div>
     </div>
 </section>
