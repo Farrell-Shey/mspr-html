223dfe2530f89fba610ba356787228df5bfe3d54
Victor Grollier <victor.grollier@gmai.com>
2020-02-06 19:41:02 +0100

    update: marque responsive

--- marque.php
+++ marque.php
@@ -1,27 +1,25 @@
 <?php require_once 'includes/header.php'; ?>
     <div class="container">
-        <div class="row">
+        <div class="titre">
+            <h1 class="caviar">CAVIAR</h1>
+        </div>
 
-            <div class="col">
-                <img class="vieux" src="public/image/client-iphone.png" alt="Vieux monsieur avec un iPhone">
-            </div>
+        <div class="image">
+            <img class="vieux" src="public/image/client-iphone.png" alt="Vieux monsieur avec un iPhone">
+        </div>
 
-            <div class="col align-self-center">
-                <h1 class="caviar">CAVIAR</h1>
-            </div>
-        </div
-            <div class="container">
-                <p class="paragraph">Nous sommes CAVIAR une marque de luxe créé en 1965 au cœur de Rome. Nous créons de
-                    nouveaux
-                    visuels aux iphones mais de façon qu'il soit unique à vos yeux et pour que vous vous différenciiez
-                    des
-                    autres . Pour créer cette différence nous utilisons des matières luxueuses telles que l'or le
-                    diamant
-                    l'émeraude le ruby mais aussi de matière rare comme un morceau de la planète mars.
-                </p>
 
-                <p class="slogan">CAVIAR la différence a l'état pur</p>
-            </div>
+        <div class="container">
+            <p class="paragraph">Nous sommes CAVIAR une marque de luxe créé en 1965 au cœur de Rome. Nous créons de
+                nouveaux
+                visuels aux iphones mais de façon qu'il soit unique à vos yeux et pour que vous vous différenciiez
+                des
+                autres . Pour créer cette différence nous utilisons des matières luxueuses telles que l'or le
+                diamant
+                l'émeraude le ruby mais aussi de matière rare comme un morceau de la planète mars.
+            </p>
+            <p class="slogan">CAVIAR la différence a l'état pur</p>
+
+        </div>
     </div>
 
-<?php require_once 'includes/footer1.php'; ?>
