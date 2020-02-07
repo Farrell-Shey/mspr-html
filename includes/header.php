ff74d801895afd6a12f43dc9e135a7af4e957bc1
Victor Grollier <victor.grollier@gmai.com>
2020-02-07 09:41:29 +0100

    update: confidentialité

--- includes/header.php
+++ includes/header.php
@@ -13,14 +13,14 @@
 <body>
 <header id="header">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">
+        <a class="navbar-brand">
             <img src="public/image/logo.png" alt="logo de caviar royal gift">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
                 aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown" style="border: #0b2e13 1px solid">
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav">
                 <li class="nav-item">
                     <a class="nav-link" href="http://localhost/mspr-html/">Accueil</a>
