655de38466fac2b90e433468ac79e7eb8b3a3f3f
Elliot <elliot.decle@gmail.com>
2020-02-07 13:28:50 +0100

    update: footer final

--- includes/footer1.php
+++ includes/footer1.php
@@ -25,18 +25,26 @@
         <div class="col-md-6 ">
 
 
-          <strong class="text-uppercase"> Vous Avez besoin d'aide?</strong>
-          <p>Suivez nous sur :</p>
+
+
             <div class="row">
                 <div class="col">
-                    <ul>
-                        <li><i class="fab fa-facebook-square"></i></li>
-                        <li><i class="fab fa-pinterest-square"></i></li>
-                    </ul>
+                    <strong class="text-uppercase">Suivez nous sur :</strong>
+                    <div class="social-media">
+                    <i class="fab fa-facebook-square fa-lg"></i>
+                    <i class="fab fa-pinterest-square fa-lg"></i>
+                    <i class="fab fa-twitter-square fa-lg"></i>
+                    <i class="fab fa-linkedin fa-lg"></i>
+                    </div>
                 </div>
                 <div class="col">
+                    <strong class="text-uppercase"> Vous Avez besoin d'aide?</strong>
+                    <div class="help">
                     <a href="contact" class="btn-contact"><img src="public/image/gear.png" alt="un rouage qui tourne" class="gear">
-                    Nous Contactez directement</a>
+                    Nous Contactez directement<img src="public/image/gear.png" alt="un rouage qui tourne" class="gear"></a>
+
+                    <a href="politique-confidentialite" class="pol-conf">Politique de Confidentialité</a>
+                    </div>
                 </div>
             </div>
 
